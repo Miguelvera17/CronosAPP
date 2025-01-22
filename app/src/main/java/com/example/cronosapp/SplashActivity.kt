916d49c2fbcd3774b206516933aa61adce7a18f8
Miguel Vera <2023_miguel.vera@iticbcn.cat>
2025-01-22 20:46:35 +0100

    intento de animación logo splashscreen

---
+++ app/src/main/java/com/example/cronosapp/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.example.cronosapp
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Encuentra el logo
+        val logoImage = findViewById<ImageView>(R.id.logoImage)
+
+        val fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.logo_animation)
+
+        logoImage.startAnimation(fadeAnimation)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
