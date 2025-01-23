8c0c705b490ebff344674873f65b6050f85c8a63
Miguel Vera <2023_miguel.vera@iticbcn.cat>
2025-01-23 17:32:27 +0100

    progresando con el logo animado

--- app/src/main/java/com/example/cronosapp/SplashActivity.kt
+++ app/src/main/java/com/example/cronosapp/SplashActivity.kt
@@ -9,25 +9,19 @@ import android.os.Looper
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import androidx.activity.ComponentActivity
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        installSplashScreen().apply {
 
-        // Encuentra el logo
-        val logoImage = findViewById<ImageView>(R.id.logoImage)
+        }
+        setContent {
 
-        val fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.logo_animation)
-
-        logoImage.startAnimation(fadeAnimation)
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
+        }
     }
 }
